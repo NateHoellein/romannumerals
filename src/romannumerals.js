417153102abb22320263b77f2ebf53eba95a53ed
Nate Hoellein <natehoellein@gmail.co>
2012-05-17 14:06:46 -0400

    Updated tests for single digit and two digit numbers.  refactored to use hashtable

--- src/romannumerals.js
+++ src/romannumerals.js
@@ -4,13 +4,40 @@ global.theApp.RomanNumerals = function() {
 
   var RomanNumerals = function() {
     this.convert = function(number) {
-      if(number === 1) {
-        return "I";
-      } else if(number === 2) {
-        return "II";
+
+      var singleDigitHash = {
+        1: "I",
+        2: "II",
+        3: "III",
+        4: "IV",
+        5: "V",
+        6: "VI",
+        7: "VII",
+        8: "VIII",
+        9: "IX",
+        10: "X"
+      };
+    
+      var tensHash = {
+        1: "X",
+        2: "XX",
+        3: "XXX",
+        4: "XL",
+        5: "L",
+        6: "LX",
+        7: "LXX",
+        8: "LXXX",
+        9: "XC"
+      };
+
+      if(number > 10) {
+        var tens = Math.floor(number / 10);
+        var ones = number % 10;
+        return tensHash[tens] + singleDigitHash[ones];
       } else {
-        return "III";
+        return singleDigitHash[number];
       }
+      
      };
   };
 
--- test/romannumeral_spec.js
+++ test/romannumeral_spec.js
@@ -3,17 +3,29 @@ var RomanNumerals = require(__dirname + '/../src/romannumerals');
 
 describe('RomanNumerals', function() {
   
-  it('1 should be I', function() {
+  it('should verify digits in ones column', function() {
     verify(1, "I");
+    verify(2, "II");
+    verify(3, "III");
+    verify(4, "IV");
+    verify(5, "V");
+    verify(6, "VI");
+    verify(7, "VII");
+    verify(8, "VIII");
+    verify(9, "IX");
   });
 
-  it('2 should be II', function() {
-    verify(2, "II");
+  it('should verify two digit numbers', function() {
+    verify(10, "X");
+    verify(11, "XI");
+    verify(21, "XXI");
+    verify(42, "XLII");
+    verify(66, "LXVI");
+    verify(59, "LIX");
+    verify(78, "LXXVIII");
+    verify(99, "XCIX");
   });
-  
-  it('3 should be III', function() {
-    verify(3, "III");
-  });
+
 
   function verify(number, expected) {
     var RomanNumerals = global.theApp.RomanNumerals();
