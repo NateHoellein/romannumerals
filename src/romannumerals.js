e35404c5e476745b27e2ceda6a05ce9a394d2fd8
Nate Hoellein <natehoellein@gmail.com>
2012-05-20 21:22:24 -0400

    More encapsulation with position hash

--- src/romannumerals.js
+++ src/romannumerals.js
@@ -9,18 +9,11 @@ global.theApp.RomanNumerals = function() {
   var RomanNumerals = function() {
     this.convert = function(number) {
     
-      var positionHash = {
-        1: defs.ones,
-        2: defs.tens,
-        3: defs.hundreds,
-        4: defs.thousands
-      };
-
       var response = "";
       var i = 1;
       while(number !== 0) {
         var digit = Math.floor(number % 10);
-        response = positionHash[i][digit] + response;
+        response = defs.positionMap[i][digit] + response;
         
         number = Math.floor(number / 10);
         (i === 4) ? i =  2 : i++;
